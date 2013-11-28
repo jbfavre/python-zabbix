03e1175fc1de5f42e42725d5155ce55f565b6081
Jean Baptiste Favre <jean-baptiste.favre@blablacar.com>
2013-11-28 19:52:31 +0100

    Update module version to 0.0.2

--- module/setup.py
+++ module/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'zabbix',
-    version = '0.0.1',
+    version = '0.0.2',
     zip_safe = True,
     
     author = 'Jean Baptiste Favre',
