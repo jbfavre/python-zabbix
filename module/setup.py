1445dbeb4590e3f53a28bd1400c6540f7bb1c028
Jean Baptiste Favre <jean-baptiste.favre@blablacar.com>
2014-02-25 13:24:20 +0100

    Bump module version to 0.0.3

--- module/setup.py
+++ module/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'protobix',
-    version = '0.0.2',
+    version = '0.0.3',
     zip_safe = True,
     
     author = 'Jean Baptiste Favre',
