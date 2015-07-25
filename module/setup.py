3b526e8492f33d3f819a76fe6769f3fe3a836f84
Jean Baptiste Favre <jean-baptiste.favre@blablacar.com>
2015-07-25 10:16:17 +0200

    Update version for pip upload

--- module/setup.py
+++ module/setup.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python
 
-from setuptools import setup, find_packages
+from distutils.core import setup
 
 setup(
     name = 'protobix',
-    version = '0.0.3',
-    zip_safe = True,
-    
-    author = 'Jean Baptiste Favre',
-    author_email = 'jean-baptiste.favre@blablacar.com',
+    packages = ['protobix'],
+    version = '0.0.6',
+
     description = 'Implementation of Zabbix Sender protocol',
     long_description = ( 'This module implements Zabbix Sender Protocol.\n'
                          'It allows to build list of items and send items and send '
                          'them as trapper.\n'
-                         'It currently supports items as well as Low Level Discovery.' ),
+                         'It currently supports items as well as Low Level Discovery.' ),    
+    author = 'Jean Baptiste Favre',
+    author_email = 'jean-baptiste.favre@blablacar.com',
     license = 'GPL',
-    url='http://www.blablacar.com/',
-    packages=['protobix'],
+    url='http://github.com/jbfavre/python-protobix/',
+    download_url = 'http://github.com/jbfavre/python-protobix/tarball/0.0.5',
+    keywords = ['monitoring','zabbix','trappers'],
+    classifiers = [],
    )
