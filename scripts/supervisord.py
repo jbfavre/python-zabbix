78aae0a8c42630c42dc349dbc34df11b6926994f
Jean Baptiste Favre <debian@jbfavre.org>
2015-09-01 15:01:57 +0200

    Update supervisord plugin for SampleProbe

--- scripts/supervisord.py
+++ scripts/supervisord.py
@@ -12,10 +12,9 @@ import sys
 import protobix
 
 
-class SupervisorServer(object):
+class SupervisorServer(protobix.SampleProbe):
 
-    __version__ = '0.0.8'
-    ZBX_CONN_ERR = 'ERR - unable to send data to Zabbix [%s]'
+    __version__ = '0.0.9'
 
     SUPERV_STAT_CHECK='sudo supervisorctl status'
     SUPERV_STATES = {
@@ -56,6 +55,17 @@ class SupervisorServer(object):
             worker_list[group_name][proc_name][proc_status] += 1
         return worker_list
 
+    def _parse_args(self):
+        # Parse the script arguments
+        # Common part
+        parser = super( SupervisorServer, self)._parse_args()
+
+        (options, args) = parser.parse_args()
+        return (options, args)
+
+    def _init_probe(self):
+        self.hostname = socket.getfqdn()
+
     def _get_metrics(self):
         data = {}
         try:
@@ -84,103 +94,6 @@ class SupervisorServer(object):
             print "CRITICAL: Could not get workers list"
             raise Exception('Fail to get supervisord infos')
         return {'supervisord.workers.discovery': data}
-
-    def _parse_args(self):
-        ''' Parse the script arguments
-        '''
-        parser = optparse.OptionParser()
-
-        parser.add_option("-d", "--dry", action="store_true",
-                              help="Performs Supervisord calls but do not send "
-                                   "anything to the Zabbix server. Can be used "
-                                   "for both Update & Discovery mode")
-        parser.add_option("-D", "--debug", action="store_true",
-                          help="Enable debug mode. This will prevent bulk send "
-                               "operations and force sending items one after the "
-                               "other, displaying result for each one")
-        parser.add_option("-v", "--verbose", action="store_true",
-                          help="When used with debug option, will force value "
-                               "display for each items managed. Beware that it "
-                               "can be pretty too much verbose, specialy for LLD")
-
-        mode_group = optparse.OptionGroup(parser, "Program Mode")
-        mode_group.add_option("--update-items", action="store_const",
-                              dest="mode", const="update_items",
-                              help="Get & send items to Zabbix. This is the default "
-                                   "behaviour even if option is not specified")
-        mode_group.add_option("--discovery", action="store_const",
-                              dest="mode", const="discovery",
-                              help="If specified, will perform Zabbix Low Level "
-                                   "Discovery on Supervisord. "
-                                   "Default is to get & send items")
-        parser.add_option_group(mode_group)
-        parser.set_defaults(mode="update_items")
-
-        general_options = optparse.OptionGroup(parser, "Supervisord "
-                                                       "configuration options")
-
-        parser.add_option_group(general_options)
-
-        zabbix_options = optparse.OptionGroup(parser, "Zabbix configuration")
-        zabbix_options.add_option("--zabbix-server", metavar="HOST", default="localhost",
-                                   help="The hostname of Zabbix server or "
-                                        "proxy, default is localhost.")
-        zabbix_options.add_option("--zabbix-port", metavar="PORT", default=10051,
-                                   help="The port on which the Zabbix server or "
-                                        "proxy is running, default is 10051.")
-        parser.add_option_group(zabbix_options)
-
-        return parser.parse_args()
-
-    def _init_container(self):
-        zbx_container = protobix.DataContainer(
-            data_type = 'items',
-            zbx_host  = self.options.zabbix_server,
-            zbx_port  = int(self.options.zabbix_port),
-            debug     = self.options.debug,
-            dryrun    = self.options.dry
-        )
-        return zbx_container
-
-    def run(self):
-        (self.options, args) = self._parse_args()
-        hostname = socket.getfqdn()
-
-        # Step 1: init container
-        try:
-            zbx_container = self._init_container()
-        except:
-            return 1
-
-        # Step 2: get data
-        try:
-            data = {}
-            if self.options.mode == "update_items":
-                zbx_container.set_type("items")
-                data[hostname] = self._get_metrics()
-                data[hostname]['supervisord.zbx_version'] = self.__version__
-
-            elif self.options.mode == "discovery":
-                zbx_container.set_type("lld")
-                data[hostname] = self._get_discovery()
-        except:
-            return 2
-
-        # Step 3: format & load data into container
-        try:
-            zbx_container.add(data)
-        except:
-            return 3
-
-        # Step 4: send container data to Zabbix server
-        try:
-            zbx_container.send(zbx_container)
-        except protobix.SenderException as zbx_e:
-            if self.options.debug:
-                print self.ZBX_CONN_ERR % zbx_e.err_text
-            return 4
-        # Everything went fine. Let's return 0 and exit
-        return 0
 
 if __name__ == '__main__':
     ret = SupervisorServer().run()
