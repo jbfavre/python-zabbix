206191bfa45281e5b6505e376ec0faf6931a6dbc
Jean Baptiste Favre <debian@jbfavre.org>
2015-09-01 15:01:23 +0200

    Update rabbitmq_server plugin for SampleProbe

--- scripts/rabbitmq_server.py
+++ scripts/rabbitmq_server.py
@@ -21,10 +21,9 @@ import socket
 import sys
 import protobix
 
-class RabbitMQServer(object):
+class RabbitMQServer(protobix.SampleProbe):
 
-    __version__ = '0.0.8'
-    ZBX_CONN_ERR = "ERR - unable to send data to Zabbix [%s]"
+    __version__ = '0.0.9'
 
     def _call_api(self, path):
         # Call the REST API and convert the results into JSON.
@@ -119,121 +118,40 @@ class RabbitMQServer(object):
                 zbx_key = "rabbitmq.queue[{0},{1},rate,{2}]"
                 zbx_key = zbx_key.format(queue['vhost'], queue['name'], item)
                 data[zbx_key] = rate_key.get('rate', 0)
+        data['rabbitmq.zbx_version'] = self.__version__
 
-        data["rabbitmq.zbx_version"] = self.__version__
-
-        return data
+        return { hostname: data }
 
     def _parse_args(self):
-        ''' Parse the script arguments'''
-        parser = optparse.OptionParser()
-
-        parser.add_option("-d", "--dry", action="store_true",
-                              help="Performs RabbitMQ API calls but do not send "
-                                   "anything to the Zabbix server. Can be used "
-                                   "for both Update & Discovery mode")
-        parser.add_option("-D", "--debug", action="store_true",
-                          help="Enable debug mode. This will prevent bulk send "
-                               "operations and force sending items one after the "
-                               "other, displaying result for each one")
-        parser.add_option("-v", "--verbose", action="store_true",
-                          help="When used with debug option, will force value "
-                               "display for each items managed. Beware that it "
-                               "can be pretty too much verbose, specialy for LLD")
-
-        mode_group = optparse.OptionGroup(parser, "Program Mode")
-        mode_group.add_option("--update-items", action="store_const",
-                              dest="mode", const="update_items",
-                              help="Get & send items to Zabbix. This is the default "
-                                   "behaviour even if option is not specified")
-        mode_group.add_option("--discovery", action="store_const",
-                              dest="mode", const="discovery",
-                              help="If specified, will perform Zabbix Low Level "
-                                   "Discovery on RabbitMQ API. "
-                                   "Default is to get & send items")
-        parser.add_option_group(mode_group)
-        parser.set_defaults(mode="update_items")
+        # Parse the script arguments
+        # Common part
+        parser = super( RabbitMQServer, self)._parse_args()
 
         general_options = optparse.OptionGroup(parser, "RabbitMQ API Configuration")
-        general_options.add_option("-H", "--host", metavar="HOST", default="localhost",
+        general_options.add_option("-H", "--host", default="localhost",
                                    help="RabbitMQ API hostname")
-        general_options.add_option("-p", "--port", help="RabbitMQ API port", default=15672)
-        general_options.add_option('--username', help='RabbitMQ API username',
-                          default='zabbix')
-        general_options.add_option('--password', help='RabbitMQ API password',
-                          default='zabbix')
+        general_options.add_option("-P", "--port", default=15672,
+                                   help="RabbitMQ API port")
+        general_options.add_option('--username', default='zabbix',
+                                   help='RabbitMQ API username')
+        general_options.add_option('--password', default='zabbix',
+                                   help='RabbitMQ API password')
         parser.add_option_group(general_options)
 
-        polling_options = optparse.OptionGroup(parser, "Zabbix configuration")
-        polling_options.add_option("--zabbix-server", metavar="HOST",
-                                   default="localhost",
-                                   help="The hostname of Zabbix server or "
-                                        "proxy, default is localhost.")
-        polling_options.add_option("--zabbix-port", metavar="PORT", default=10051,
-                                   help="The port on which the Zabbix server or "
-                                        "proxy is running, default is 10051.")
-        parser.add_option_group(polling_options)
+        (options, args) = parser.parse_args()
+        return (options, args)
 
-        return parser.parse_args()
-
-    def _init_container(self):
-        zbx_container = protobix.DataContainer(
-            data_type = 'items',
-            zbx_host  = self.options.zabbix_server,
-            zbx_port  = int(self.options.zabbix_port),
-            debug     = self.options.debug,
-            dryrun    = self.options.dry
-        )
-        return zbx_container
-
-    def run(self, config_file='/etc/zabbix/rabbitmq_server.yaml'):
-        (self.options, args) = self._parse_args()
+    def _init_probe(self):
         if self.options.host == 'localhost':
             self.options.host = socket.getfqdn()
-        hostname = self.options.host
-
-        # Step 1: init container
-        try:
-            zbx_container = self._init_container()
-        except:
-            return 1
-
-        # Step 2: get data
-        try:
-            ''' Load config file '''
-            with open(config_file, 'r') as f:
-                config = yaml.load(f)
-            self.queue_limits = config['queues_limits']
-            ''' Prepare regex pattern for queue exclusion '''
-            pattern_string = '|'.join(config['exclude_pattern'])
-            self.exclude_patterns = re.compile(pattern_string)
-
-            data = {}
-            if self.options.mode == "update_items":
-                zbx_container.set_type("items")
-                data[hostname] = self._get_metrics()
-
-            elif self.options.mode == "discovery":
-                zbx_container.set_type("lld")
-                data[hostname] = self._get_discovery()
-        except:
-            return 2
-
-        # Step 3: format & load data into container
-        try:
-            zbx_container.add(data)
-        except:
-            return 3
-
-        # Step 4: send container data to Zabbix server
-        try:
-            zbx_container.send(zbx_container)
-        except protobix.SenderException as zbx_e:
-            if self.options.debug:
-                print self.ZBX_CONN_ERR % zbx_e.err_text
-            return 4
-        # Everything went fine. Let's return 0 and exit
-        return 0
+        self.hostname = self.options.host
+        ''' Load config file '''
+        with open(self.options.config, 'r') as f:
+          config = yaml.load(f)
+        self.queue_limits = config['queues_limits']
+        ''' Prepare regex pattern for queue exclusion '''
+        pattern_string = '|'.join(config['exclude_pattern'])
+        self.exclude_patterns = re.compile(pattern_string)
 
 if __name__ == '__main__':
     ret = RabbitMQServer().run()
