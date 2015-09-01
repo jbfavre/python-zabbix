6fc34826f513940aab9d18d23b2c3f1e3fe0edfc
Jean Baptiste Favre <debian@jbfavre.org>
2015-09-01 15:01:14 +0200

    Update php_fpm plugin for SampleProbe

--- scripts/php_fpm.py
+++ scripts/php_fpm.py
@@ -480,10 +480,9 @@ class FCGIApp(object):
 # PHP-FPM plugin
 ################################################################################
 
-class PhpFpm(object):
+class PhpFpm(protobix.SampleProbe):
 
-    __version__ = '0.0.8'
-    ZBX_CONN_ERR = 'ERR - unable to send data to Zabbix [%s]'
+    __version__ = '0.0.9'
 
     PHP_POOL_CONFIG_KEYS = [
       'pm.start_servers',
@@ -535,7 +534,31 @@ class PhpFpm(object):
         ret = fcgi(env, 0)
         return ret
 
-    def _get_metrics(self):
+    def _get_pools_config(self):
+        f = []
+        pool_config = ConfigParser.ConfigParser()
+        root_path = '/etc/php5/fpm/pool.d'
+        poolfilelist = [
+            os.path.join(root_path, f) for f in
+            os.listdir(root_path)
+            if os.path.isfile(os.path.join(root_path, f))
+        ]
+        pool_config.read(poolfilelist)
+        pool_config_dict = simplejson.loads(simplejson.dumps(pool_config._sections))
+        return pool_config_dict
+
+    def _parse_args(self):
+        # Parse the script arguments
+        # Common part
+        parser = super( PhpFpm, self)._parse_args()
+
+        (options, args) = parser.parse_args()
+        return parser.parse_args()
+
+    def _init_probe(self):
+        self.hostname = socket.getfqdn()
+
+    def _get_metrics(self, hostname):
         data = {}
         pool_list = self._get_pools_config()
         for pool in pool_list:
@@ -605,125 +628,16 @@ class PhpFpm(object):
                 zbx_key = zbx_key.format(pool)
                 data[zbx_key] = 0
                 pass
-        return data
+            data['php-fpm.zbx_version'] = self.__version__
+        return { hostname: data }
 
-    def _get_discovery(self):
+    def _get_discovery(self, hostname):
         data = []
         pool_list = self._get_pools_config()
         for pool in pool_list:
           element = { '{#PHPFPMPOOLNAME}': pool }
           data.append(element)
         return {'php-fpm.pools.discovery': data}
-
-    def _get_pools_config(self):
-        f = []
-        pool_config = ConfigParser.ConfigParser()
-        root_path = '/etc/php5/fpm/pool.d'
-        poolfilelist = [
-            os.path.join(root_path, f) for f in
-            os.listdir(root_path)
-            if os.path.isfile(os.path.join(root_path, f))
-        ]
-        pool_config.read(poolfilelist)
-        pool_config_dict = simplejson.loads(simplejson.dumps(pool_config._sections))
-        return pool_config_dict
-
-    def _parse_args(self):
-        ''' Parse the script arguments
-        '''
-        parser = optparse.OptionParser()
-
-        parser.add_option("-d", "--dry", action="store_true",
-                              help="Performs Supervisord calls but do not send "
-                                   "anything to the Zabbix server. Can be used "
-                                   "for both Update & Discovery mode")
-        parser.add_option("-D", "--debug", action="store_true",
-                          help="Enable debug mode. This will prevent bulk send "
-                               "operations and force sending items one after the "
-                               "other, displaying result for each one")
-        parser.add_option("-v", "--verbose", action="store_true",
-                          help="When used with debug option, will force value "
-                               "display for each items managed. Beware that it "
-                               "can be pretty too much verbose, specialy for LLD")
-
-        mode_group = optparse.OptionGroup(parser, "Program Mode")
-        mode_group.add_option("--update-items", action="store_const",
-                              dest="mode", const="update_items",
-                              help="Get & send items to Zabbix. This is the default "
-                                   "behaviour even if option is not specified")
-        mode_group.add_option("--discovery", action="store_const",
-                              dest="mode", const="discovery",
-                              help="If specified, will perform Zabbix Low Level "
-                                   "Discovery on Supervisord. "
-                                   "Default is to get & send items")
-        parser.add_option_group(mode_group)
-        parser.set_defaults(mode="update_items")
-
-        general_options = optparse.OptionGroup(parser, "Supervisord "
-                                                       "configuration options")
-
-        parser.add_option_group(general_options)
-
-        zabbix_options = optparse.OptionGroup(parser, "Zabbix configuration")
-        zabbix_options.add_option("--zabbix-server", metavar="HOST", default="localhost",
-                                   help="The hostname of Zabbix server or "
-                                        "proxy, default is localhost.")
-        zabbix_options.add_option("--zabbix-port", metavar="PORT", default=10051,
-                                   help="The port on which the Zabbix server or "
-                                        "proxy is running, default is 10051.")
-        parser.add_option_group(zabbix_options)
-
-        return parser.parse_args()
-
-    def _init_container(self):
-        zbx_container = protobix.DataContainer(
-            data_type = 'items',
-            zbx_host  = self.options.zabbix_server,
-            zbx_port  = int(self.options.zabbix_port),
-            debug     = self.options.debug,
-            dryrun    = self.options.dry
-        )
-        return zbx_container
-
-    def run(self):
-        (self.options, args) = self._parse_args()
-        hostname = socket.getfqdn()
-
-        # Step 1: init container
-        try:
-            zbx_container = self._init_container()
-        except:
-            return 1
-
-        # Step 2: get data
-        try:
-            data = {}
-            if self.options.mode == "update_items":
-                zbx_container.set_type("items")
-                data[hostname] = self._get_metrics()
-                data[hostname]['php-fpm.zbx_version'] = self.__version__
-
-            elif self.options.mode == "discovery":
-                zbx_container.set_type("lld")
-                data[hostname] = self._get_discovery()
-        except:
-            return 2
-
-        # Step 3: format & load data into container
-        try:
-            zbx_container.add(data)
-        except:
-            return 3
-
-        # Step 4: send container data to Zabbix server
-        try:
-            zbx_container.send(zbx_container)
-        except protobix.SenderException as zbx_e:
-            if self.options.debug:
-                print self.ZBX_CONN_ERR % zbx_e.err_text
-            return 4
-        # Everything went fine. Let's return 0 and exit
-        return 0
 
 if __name__ == '__main__':
     ret = PhpFpm().run()
