8e11e2b75baf89fa753f0bff3cffb28ae01219a6
Jean Baptiste Favre <debian@jbfavre.org>
2016-01-11 15:33:52 +0100

    WIP: Improve Elasticsearch probe

--- scripts/elasticsearch_server.py
+++ scripts/elasticsearch_server.py
@@ -21,46 +21,263 @@ class ElasticsearchServer(protobix.SampleProbe):
       'yellow': 1,
       'red': 2
     }
-    searchkeys = [
-      'query_total',
-      'fetch_time_in_millis',
-      'fetch_total',
-      'fetch_time',
-      'query_current',
-      'fetch_current',
-      'query_time_in_millis'
-    ]
-    getkeys = [
-      'missing_total',
-      'exists_total',
-      'current',
-      'time_in_millis',
-      'missing_time_in_millis',
-      'exists_time_in_millis',
-      'total'
-    ]
-    docskeys = [
-      'count',
-      'deleted'
-    ]
-    indexingkeys = [
-      'delete_time_in_millis',
-      'index_total',
-      'index_current',
-      'delete_total',
-      'index_time_in_millis',
-      'delete_current'
-    ]
-    storekeys = [
-      'size_in_bytes',
-      'throttle_time_in_millis'
-    ]
-    cachekeys = [
-      'filter_size_in_bytes',
-      'field_size_in_bytes',
-      'field_evictions'
-    ]
-    clusterkeys = searchkeys + getkeys + docskeys + indexingkeys + storekeys
+    ES_INDICES_KEYS = {
+        'suggest': [
+            'current',
+            'time_in_millis',
+            'total'
+        ],
+        'search': [
+            'fetch_current',
+            'fetch_time_in_millis',
+            'fetch_total',
+            'query_current',
+            'query_time_in_millis',
+            'query_total',
+            'open_contexts'
+        ],
+        'fielddata': [
+            'evictions',
+            'memory_size_in_bytes'
+        ],
+        'get': [
+            'current',
+            'exists_time_in_millis',
+            'exists_total',
+            'missing_time_in_millis',
+            'missing_total',
+            'time_in_millis',
+            'total'
+        ],
+        'translog': [
+            'operations',
+            'size_in_bytes'
+        ],
+        'docs': [
+            'count',
+            'deleted'
+        ],
+        'segments': [
+            'count',
+            'fixed_bit_set_memory_in_bytes',
+            'index_writer_max_memory_in_bytes',
+            'index_writer_memory_in_bytes',
+            'memory_in_bytes',
+            'version_map_memory_in_bytes'
+        ],
+        'flush': [
+            'total',
+            'total_time_in_millis'
+        ],
+        'indexing': [
+            'delete_current',
+            'delete_time_in_millis',
+            'delete_total',
+            'index_current',
+            'index_time_in_millis',
+            'index_total',
+            'is_throttled',
+            'noop_update_total',
+            'throttle_time_in_millis'
+        ],
+        'refresh': [
+            'total_time_in_millis',
+            'total'
+        ],
+        'query_cache': [
+            'evictions',
+            'hit_count',
+            'memory_size_in_bytes',
+            'miss_count'
+        ],
+        'warmer': [
+            'current',
+            'total',
+            'total_time_in_millis'
+        ],
+        'filter_cache': [
+            'evictions',
+            'memory_size_in_bytes'
+        ],
+        'percolate': [
+            'current',
+            'memory_size',
+            'memory_size_in_bytes',
+            'queries',
+            'time_in_millis',
+            'total'
+        ],
+        'merges': [
+            'current',
+            'current_docs',
+            'current_size_in_bytes',
+            'total',
+            'total_docs',
+            'total_size_in_bytes',
+            'total_time_in_millis'
+        ],
+        'store': [
+            'size_in_bytes',
+            'throttle_time_in_millis'
+        ],
+        'recovery': [
+            'current_as_source',
+            'current_as_target',
+            'throttle_time_in_millis'
+        ]
+    }
+    ES_JVM_KEYS = {
+        'mem': [
+            'heap_used_in_bytes',
+            'heap_used_percent',
+            'heap_committed_in_bytes',
+            'heap_max_in_bytes',
+            'non_heap_used_in_bytes',
+            'non_heap_committed_in_bytes'
+        ],
+        'threads': [
+            'count',
+            'peak_count'
+        ]
+    }
+    ES_THREAD_POOLS_KEYS = {
+        "percolate": [
+           'threads',
+           'queue',
+           'active',
+           'rejected',
+           'largest',
+           'completed'
+        ],
+        'fetch_shard_started': [
+           'threads',
+           'queue',
+           'active',
+           'rejected',
+           'largest',
+           'completed'
+        ],
+        'listener': [
+           'threads',
+           'queue',
+           'active',
+           'rejected',
+           'largest',
+           'completed'
+        ],
+        'index': [
+           'threads',
+           'queue',
+           'active',
+           'rejected',
+           'largest',
+           'completed'
+        ],
+        'refresh': [
+           'threads',
+           'queue',
+           'active',
+           'rejected',
+           'largest',
+           'completed'
+        ],
+        'suggest': [
+           'threads',
+           'queue',
+           'active',
+           'rejected',
+           'largest',
+           'completed'
+        ],
+        'generic': [
+           'threads',
+           'queue',
+           'active',
+           'rejected',
+           'largest',
+           'completed'
+        ],
+        'warmer': [
+           'threads',
+           'queue',
+           'active',
+           'rejected',
+           'largest',
+           'completed'
+        ],
+        'search': [
+           'threads',
+           'queue',
+           'active',
+           'rejected',
+           'largest',
+           'completed'
+        ],
+        'flush': [
+           'threads',
+           'queue',
+           'active',
+           'rejected',
+           'largest',
+           'completed'
+        ],
+        'optimize': [
+           'threads',
+           'queue',
+           'active',
+           'rejected',
+           'largest',
+           'completed'
+        ],
+        'fetch_shard_store': [
+           'threads',
+           'queue',
+           'active',
+           'rejected',
+           'largest',
+           'completed'
+        ],
+        'management': [
+           'threads',
+           'queue',
+           'active',
+           'rejected',
+           'largest',
+           'completed'
+        ],
+        'get': [
+           'threads',
+           'queue',
+           'active',
+           'rejected',
+           'largest',
+           'completed'
+        ],
+        'merge': [
+           'threads',
+           'queue',
+           'active',
+           'rejected',
+           'largest',
+           'completed'
+        ],
+        'bulk': [
+           'threads',
+           'queue',
+           'active',
+           'rejected',
+           'largest',
+           'completed'
+        ],
+        'snapshot': [
+           'threads',
+           'queue',
+           'active',
+           'rejected',
+           'largest',
+           'completed'
+        ]
+    }
     returnval = None
 
     def _init_probe(self):
@@ -97,16 +314,10 @@ class ElasticsearchServer(protobix.SampleProbe):
         data[self.discovery_key].append(cluster_list)
         return { self.hostname: data }
 
-    def _get_metrics(self):
+    def _get_cluster_metrics(self):
         data = {}
-        cluster_data = self.es.cluster.health(request_timeout=1)
+        cluster_data = self.es.cluster.health(request_timeout=1, local=True)
         cluster_name = cluster_data['cluster_name']
-        # Is the node cluster master ?
-        whois_master = self.es.cat.master(master_timeout=1,local=True).split(" ")
-        zbx_key = 'elasticsearch.cluster[{0},is_master]'
-        zbx_key = zbx_key.format(cluster_name)
-        data[zbx_key] = 1 if whois_master[3] == self.hostname else 0
-        # Cluster wide metrics
         for key in cluster_data:
           if key != 'cluster_name':
             zbx_key = 'elasticsearch.cluster[{0},{1}]'
@@ -117,44 +328,75 @@ class ElasticsearchServer(protobix.SampleProbe):
               data[zbx_key] = self.ES_CLUSTER_BOOL_MAPPING[cluster_data[key]]
             else:
               data[zbx_key] = cluster_data[key]
-        cluster_data = self.es.cluster.stats()['indices']
-        zbx_key = 'elasticsearch.indices.{0}'
-        for key in ['count', 'fielddata', 'filter_cache']:
-          if key == 'count':
-            real_key = zbx_key.format(key + '[' + cluster_name + ']',)
-            data[real_key] = int(cluster_data[key])
-          else:
-            real_key = zbx_key.format(key + '[' + cluster_name + ',evictions]')
-            data[real_key] = int(cluster_data[key]['evictions'])
-            real_key = zbx_key.format(key + '[' + cluster_name + ',memory_size_in_bytes]')
-            data[real_key] = int(cluster_data[key]['memory_size_in_bytes'])
-        indice_data = self.es.indices.stats()['_all']['total']
-        for key in ['search', 'indexing', 'docs', 'refresh', 'merges']:
-          zbx_key = 'elasticsearch.indices.{0}'
-          if key == 'count':
-            real_key = zbx_key.format('docs')
-            data[real_key] = int(indice_data[key]['count']) 
-          elif key in ['refresh','merges']:
-            real_key = zbx_key.format(key + '[' + cluster_name + ',total]')
-            data[real_key] = int(indice_data[key]['total'])
-            real_key = zbx_key.format(key + '[' + cluster_name + ',total_time_in_millis]')
-            data[real_key] = int(indice_data[key]['total_time_in_millis'])
-          elif key == 'search':
-            real_key = zbx_key.format(key + '[' + cluster_name + ',query_total]')
-            data[real_key] = int(indice_data[key]['query_total'])
-            real_key = zbx_key.format(key + '[' + cluster_name + ',query_time_in_millis]')
-            data[real_key] = int(indice_data[key]['query_time_in_millis'])
-            real_key = zbx_key.format(key + '[' + cluster_name + ',query_current]')
-            data[real_key] = int(indice_data[key]['query_current'])
-          elif key == 'indexing':
-            real_key = zbx_key.format(key + '[' + cluster_name + ',index_total]')
-            data[real_key] = int(indice_data[key]['index_total'])
-            real_key = zbx_key.format(key + '[' + cluster_name + ',index_time_in_millis]')
-            data[real_key] = int(indice_data[key]['index_time_in_millis'])
-          else:
-            ''' Should never be there '''
-        data['elasticsearch.zbx_version'] = self.__version__
-        return { self.hostname: data }
+
+        # Check wether current node is master for the cluster
+        whois_master = self.es.cat.master(local=True).split(" ")
+        self.is_master = True if whois_master[3] == self.hostname else False
+        zbx_key = 'elasticsearch.cluster[{0},is_master]'
+        zbx_key = zbx_key.format(cluster_name)
+        data[zbx_key] = self.ES_CLUSTER_BOOL_MAPPING[self.is_master]
+        return data, cluster_name
+
+    def _get_nodes_stats(self, cluster_name):
+        data = {}
+        nodes_stats = self.es.nodes.stats()
+        for node in nodes_stats['nodes']:
+            # Skip non data nodes
+            if 'data' in nodes_stats['nodes'][node]['attributes'] and \
+               nodes_stats['nodes'][node]['attributes']['data'] == 'false':
+                continue
+            hostname = nodes_stats['nodes'][node]['name']
+            data[hostname] = {}
+            # Get indices stats
+            indices_stats = nodes_stats['nodes'][node]['indices']
+            data[hostname].update(self._get_indices_stats(indices_stats))
+            # Get jvm stats
+            jvm_stats = nodes_stats['nodes'][node]['jvm']
+            data[hostname].update(self._get_jvm_stats(jvm_stats))
+            thread_pool_stats = nodes_stats['nodes'][node]['thread_pool']
+            data[hostname].update(self._get_thread_pools_stats(thread_pool_stats))
+        return data
+
+    def _get_indices_stats(self, raw_data):
+        data = {}
+        zbx_key = 'elasticsearch.indices[{0},{1}]'
+        for key in self.ES_INDICES_KEYS:
+            for metric in self.ES_INDICES_KEYS[key]:
+                real_key = zbx_key.format(key, metric)
+                data[real_key] = raw_data[key][metric]
+        return data
+
+    def _get_jvm_stats(self, raw_data):
+        data = {}
+        zbx_key = 'elasticsearch.jvm[{0},{1}]'
+        for key in self.ES_JVM_KEYS:
+            for metric in self.ES_JVM_KEYS[key]:
+                real_key = zbx_key.format(key, metric)
+                data[real_key] = raw_data[key][metric]
+        return data
+
+    def _get_thread_pools_stats(self, raw_data):
+        data = {}
+        zbx_key = 'elasticsearch.thread_pools[{0},{1}]'
+        for key in self.ES_THREAD_POOLS_KEYS:
+            for metric in self.ES_THREAD_POOLS_KEYS[key]:
+                real_key = zbx_key.format(key, metric)
+                data[real_key] = raw_data[key][metric]
+        return data
+
+    def _get_metrics(self):
+        # Get health status
+        data = { self.hostname: {} }
+        data[self.hostname], cluster_name = self._get_cluster_metrics()
+        if data[self.hostname]['elasticsearch.cluster['+cluster_name+',is_master]'] == 1:
+            nodes_data = self._get_nodes_stats(cluster_name)
+            for host in nodes_data:
+                if host in data:
+                    data[host].update(nodes_data[host])
+                else:
+                    data[host] = nodes_data[host]
+        data[self.hostname]['elasticsearch.zbx_version'] = self.__version__
+        return data
 
 if __name__ == '__main__':
     ret = ElasticsearchServer().run()
